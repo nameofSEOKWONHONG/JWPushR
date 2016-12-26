55dd38dc82ced61d2c9547b0ba8f834aa7f6e8df
ZZIK_05 <ZZIK_05@ZZIK_05>
2016-12-26 19:57:35 +0900

    added comment.

--- JWPush.PushSharp/Models/Apns.cs
+++ JWPush.PushSharp/Models/Apns.cs
@@ -14,13 +14,32 @@ namespace JWPush.Models
 
     public sealed class Apns : BasePushModel
     {
+
+        /// <summary>
+        ///장치 확인
+        ///공백 제거된 16진수 문자열
+        /// </summary>
         public string DeviceToken { get; set; }
+
+
         public DateTime? Expiration { get; set; }
         public int Identifier { get; }
         public bool LowPriority { get; set; }
 
         /// <summary>
-        /// json string
+        /// json string : 
+        /// {
+        ///     "aps" : { "alert" : { // 스트링 혹은 딕셔너리 형태로 값이 들어갈 수 있으며 스트링으로 할 경우 보내고자하는 메시지를 적어 넣어주시면 됩니다.
+        ///         "body" : "내용을 적어넣으세요", //전달할 내용을 입력합니다.
+        ///         "action-loc-key" : "PLAY", //LocalizedString의 키값을 참조해 AlertView의 View Detail이라는 버튼을 언어에 맞게 변경시켜줍니다.
+        ///         "loc-key" : "CONTENT", //LocalizedString의 키값을 참조해 내용을 채워줍니다.
+        ///         "loc-args" : [ "Jenna", "Frank" ]
+        ///     },
+        ///     "badge" : 5, //배지의 숫자를 입력합니다.
+        ///     },
+        ///     "acme1" : "bar",
+        ///     "acme2" : [ "bang", "whiz" ] //커스터마이징 변수값으로 임의의 문자열로 키와 값을 만들어 스트링 혹은 배열의 형태로 전달 할 수 있습니다.
+        /// }
         /// </summary>
         public string Payload { get; set; }
 
--- JWPush.PushSharp/Models/Gcm.cs
+++ JWPush.PushSharp/Models/Gcm.cs
@@ -15,19 +15,54 @@ namespace JWPush.Models
 
     public sealed class Gcm
     {
+        /// <summary>
+        /// 메시지 타입을 그룹화하는 기능으로 해당 단말이 off 일 경우 가장 최신 메세지만 전달되는 형태
+        /// </summary>
         public string CollapseKey { get; set; }        
-        public bool? ContentAvailable { get; set; }        
-        public string Data { get; set; }        
-        public bool? DelayWhileIdle { get; set; }        
-        public bool? DryRun { get; set; }        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool? ContentAvailable { get; set; }
+
+        /// <summary>
+        /// 4kb 미만의 메세지 Json형태(key-value pair)
+        /// </summary>    
+        public string Data { get; set; }
+
+        /// <summary>
+        /// message 가 바로 전송되는 것이 아니라, phone 이 on 되었을 때 collapse_key 의 가장 마지막 메시지만 전송되도록 설정
+        /// </summary>
+        public bool? DelayWhileIdle { get; set; }
+
+        /// <summary>
+        /// true일 경우 dry run mode가 활성화됩니다. 기본 값은 false 입니다. dry mode란, code가 로컬에서 실행은 되지만, GA 서버로는 데이터를 전송하지 않는 mode입니다. 이것은 기록된 데이터를 더럽히지 않고 GA SDK에 대한 호출을 디버깅하는데 유용합니다.
+        /// </summary>
+        public bool? DryRun { get; set; } = false;
+
         public string MessageId { get; }        
-        public string Notification { get; set; }        
+        public string Notification { get; set; }    
+        
+        //not use    
         public string NotificationKey { get; set; }
+
+        /// <summary>
+        /// 우선순위
+        /// </summary>
         public int? Priority { get; set; } //Normal = 5, High = 10        
+
+        /// <summary>
+        /// 필수사항 array로 1~1000 개의 아이디 입력 가능
+        /// </summary>
         public List<string> RegistrationIds { get; set; }        
         public string RestrictedPackageName { get; set; }        
-        public object Tag { get; set; }        
-        public int? TimeToLive { get; set; }        
+        public object Tag { get; set; }
+
+        /// <summary>
+        ///  단말이 off 일 때 GCM storage 에서 얼마나 저장되어야 하는지 설정하는 값으로 collapse_key 와 반드시 함께 설정되야 함
+        /// </summary>  
+        public int? TimeToLive { get; set; }   
+             
         public string To { get; set; }
     }
 }
